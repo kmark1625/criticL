a4fccc6a00a39254532d19a08e7a4d69733896ca
Noah Heinrich <noahheinrich@gmail.com>
2016-01-04 16:32:00 -0600

    Add mvp user page

--- criticL/db/schema.rb
+++ criticL/db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160104165632) do
     t.integer  "upvotes",         default: 0
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
-
   end
 
   create_table "votes", force: :cascade do |t|
