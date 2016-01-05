236d0cfafac7376527b3759fbf856a644966d984
Noah Heinrich <noahheinrich@gmail.com>
2016-01-05 14:01:19 -0600

    Add some tests for movies controller

---
+++ criticL/spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe MoviesController do
+  let!(:movie) { Movie.create!(title: "Deadpool", release_date: "12-2-16", summary: "Chimichangas!" )}
+
+  describe "GET#index" do
+    it "assigns all movies as @movies" do
+      get(:index)
+      expect(assigns(:movies).first).to be_a(Movie)
+    end
+  end
+
+  # describe "GET#show" do
+  #   it "assigns requested movie as @movie" do
+  #     get :movie, { id: movie.id }
+  #     expect(assigns(:movie)).to eq(movie)
+  #   end
+  # end
+end
