f7f2b6bd2317d124e12fa1673628240fbd328422
Noah Heinrich <noahheinrich@gmail.com>
2016-01-06 10:55:44 -0600

    Add pagination to search feature

--- criticL/app/controllers/movies_controller.rb
+++ criticL/app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
 
   def search
     search = params[:search]
-    @movies = Movie.search_by_title(search).order(avg_rating: :desc).last(10)
+    @movies = Movie.order(avg_rating: :desc).search_by_title(search).paginate(:page => params[:page], :per_page => 10)
     @genres = Genre.all
     render "index"
   end
