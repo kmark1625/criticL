8a1636095be197281b33b59c0c52dfc4e5339800
Noah Heinrich <noahheinrich@gmail.com>
2016-01-06 09:45:37 -0600

    Add pagination to index page

--- criticL/app/controllers/movies_controller.rb
+++ criticL/app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.order(avg_rating: :desc).first(10)
+    @movies = Movie.order(avg_rating: :desc).paginate(:page => params[:page], :per_page => 10)
     @genres = Genre.all
   end
 
